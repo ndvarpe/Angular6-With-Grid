524e694bb81f80fb20c1cb875a7ad4de8a890547
nvarpe <nitin.varpe@wolterskluwer.com>
2018-08-24 18:06:29 +0530

    Added new boostrap version with funnel component.

--- Angular 2/ClientApp/src/app/app.module.ts
+++ Angular 2/ClientApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -24,10 +25,12 @@ import { FunnelComponent } from './components/funnel/funnel.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    NgbModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'funnel', component: FunnelComponent },
     ])
   ],
   providers: [],
--- Angular 2/ClientApp/src/app/components/funnel/funnel.component.ts
+++ Angular 2/ClientApp/src/app/components/funnel/funnel.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-funnel',
   templateUrl: './funnel.component.html',
-  styleUrls: ['./funnel.component.css']
+  styleUrls: ['./funnel.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class FunnelComponent implements OnInit {
+  closeResult: string;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
+    console.log('Component');
+  }
+
+  openBackDropCustomClass(content) {
+    this.modalService.open(content, { backdropClass: 'light-blue-backdrop' });
+  }
+
+  openWindowCustomClass(content) {
+    this.modalService.open(content, { windowClass: 'dark-modal' });
+  }
+
+  openSm(content) {
+    this.modalService.open(content, { size: 'sm' });
+  }
+
+  openLg(content) {
+    this.modalService.open(content, { size: 'lg' });
+  }
+
+  openVerticallyCentered(content) {
+    this.modalService.open(content, { centered: true });
   }
 
 }
